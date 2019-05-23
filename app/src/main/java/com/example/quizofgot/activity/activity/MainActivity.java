68a550923684e11e9959b3edc4b86f1481f2a2ac
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2019-05-23 00:20:51 -0300

    Configuração de som funcionando

--- app/src/main/java/com/example/quizofgot/activity/activity/MainActivity.java
+++ app/src/main/java/com/example/quizofgot/activity/activity/MainActivity.java
@@ -1,49 +1,38 @@
 package com.example.quizofgot.activity.activity;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.quizofgot.R;
+import com.example.quizofgot.activity.activity.model.Sound;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MediaPlayer mp = new MediaPlayer();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Cursor cursor = null;
-
         try {
             SQLiteDatabase db = openOrCreateDatabase("app", MODE_PRIVATE, null);
-//            db.execSQL("DROP TABLE ranks");
             db.execSQL("CREATE TABLE IF NOT EXISTS ranks(" +
                     "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "nome VARCHAR, " +
                     "pontuacao INT(2)" +
                     ")");
-//            db.execSQL("INSERT INTO ranks(nome, pontuacao) VALUES('matheus', 15)");
-            cursor = db.rawQuery("SELECT * FROM ranks", null);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        mp.stop();
-        mp = MediaPlayer.create(this, R.raw.got_sound);
-        mp.start();
-        mp.setLooping(true);
+        Sound.playAudio(this, R.raw.got_sound);
     }
 
     public void btnJogar(View view) {
         Intent intent = new Intent(this, TemasActivity.class);
+        finish();
         startActivity(intent);
     }
 }
